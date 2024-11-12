e5d8aaed3de8b70ad2065563e5c15c3052e65ae7
Satya <89769507+SatyaRy@users.noreply.github.com>
2024-11-12 12:08:15 +0700

    finally

--- Frontend/src/pages/Project.jsx
+++ Frontend/src/pages/Project.jsx
@@ -1,8 +1,7 @@
 import "../style/Project.scss"
 import { IoMdLink } from "react-icons/io";
 import { FaArrowRightLong } from "react-icons/fa6";
-import iphone from "../assets/technology/ui.png"
-import iphone1 from "../assets/technology/ui1.png"
+import {delay, inView, motion} from "framer-motion"
 export const ProjectFeature = ({firstUI,secondUI})=>{
     return (
             <div className ="projectUI">
@@ -17,12 +16,28 @@ export const ProjectFeature = ({firstUI,secondUI})=>{
 }
 
 export const ProjectDetail = ({name, detail}) =>{
+    const animate = {
+        hidden: {
+            opacity: 0,
+            x: -100,
+        },
+        visible: {
+            opacity: 1,
+            x: 0,
+            transition: {
+               delay: 0.1,
+               duration: 1,
+            }
+        }
+    }
     return(
             <div className ="projectDescription">
-                <span>{name}</span>
-                <span>
+                <motion.span 
+                viewport={{ once: true }}
+                initial={{x:-100,opacity:0}} whileInView={{x:0,opacity:1,transition:{duration: 1}}}>{name}</motion.span>
+                <motion.span initial={{y: 100}} whileInView = {{y: 0,opacity:1, transition:{duration:1 }}} viewport={{ once: true }}>
                    {detail}
-                </span>
+                </motion.span>
                 <button className="readMore">
                     <span className="readButton">Read Me</span>     
                     <FaArrowRightLong color="#8892b0" fontSize={"1.2rem"}/>
--- Frontend/src/pages/Skill.jsx
+++ Frontend/src/pages/Skill.jsx
@@ -1,17 +1,44 @@
 import "../style/Skill.scss"
 import { SkillDetail,framework } from "../data/Detail"
+import {easeIn, motion} from "framer-motion"
 export default function Skill(){
+    const animateParent  = {
+        start:{
+            transition: {
+                staggerChildren: 0.2
+            }
+        },
+        end:{
+            transition:{
+                staggerChildren: 0.25
+            }
+        }
+    }
+    const animateChild ={
+        start: {
+            y: 10
+        },
+        end: {
+            y: -50
+        }
+    }
+    const tran = {
+        duration: 0.4,
+        yoyo: Infinity,
+        ease: "linear",
+    }
     return(
             <div className ="mySkills">
                 <span className="skillHead">My Skills</span>
-                <div className ="skillBox">
+                <motion.div className ="skillBox" variants={animateParent} initial="start" animate="end">
                     {SkillDetail.map((value)=>(
-                        <div className ="skillIcon" key={value.id}>
-                            <img src={value.icon} alt="" />
-                            <span>{value.name}</span>
-                        </div>
+                        <motion.div
+                         variants={animateChild} transition={tran}
+                         className ="skillIcon" key={value.id}>
+                                <img src={value.icon} alt="" />
+                        </motion.div>
                     ))}
-                </div>
+                </motion.div>
             </div>  
     )
 }
